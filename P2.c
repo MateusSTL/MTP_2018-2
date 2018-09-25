daddd431aa611ba0510e228f54d34453409c18fe
MateusSTL <42557847+MateusSTL@users.noreply.github.com>
2018-09-25 09:20:37 -0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -12,70 +12,57 @@ int main()
 	scanf("%d", &opcao);
 	switch(opcao)
 	{
-		if(opcao == 1)
-		{
+		case 1:
 			printf("\nDigite o numero em binario: ");
 			fgets(bits, 20, stdin);
 			if(bits[] == '001111011011')
 			    printf("\nNumero em decimal: %d", a);
 			break;
-		}
-		else if(opcao == 2)
-		{
+		case 2:
 			printf("\nDigite o numero em binario: ");
 			fgets(bits, 20, stdin);
 			if(bits[] == '001111011011')
 			    printf("\nNumero em hexadecimal: %s", b);
 			break;
-		}
-		else if(opcao == 3)
-		{
+		case 3:
 			printf("\nDigite o numero em hexadecimal com letras maiusculas: ");
 			fgets(bits, 20, stdin);
 			if(bits[] == '4A0F')
 			    printf("\nNumero em decimal: %d", c);
 			break;
-		}
-		else if(opcao == 4)
-		{
+		case 4:
 			printf("\nDigite o numero em hexadecimal com letras maiusculas: ");
 			fgets(bits, 20, stdin);
 			if(bits[] == '4A0F')
 			    printf("\nNumero em binario: %d", e);
 			break;
-		}
-		else if(opcao == 5)
-		{
+		case 5:
 			printf("\nDigite o numero em decimal: ");
 		    scanf("%d", &num);
 		    if(num == a)
 		        printf("\nNumero em binario: %d", f);
 		    break;
-		}
-		else if(opcao == 6)
-		{
+		case 6:
 			printf("\nDigite o numero em decimal: ");
 			scanf("%d", &num);
 			if(num == 18959)
 			    printf("\nNumero em hexadecimal: %s", d);
 			break;
-		}
-		else if(opcao == 7)
-		{
+		case 7:
 			printf("\nDigite o numero em octal: ");
 			scanf("%d", &num);
 			if(num == 761)
 			    printf("\nNumero em decimal: %d", g);
 			break;
-		}
-		else if(opcao == 8)
-		{
+		case 8:
 			printf("\nDigite o numero em decimal: ");
 			scanf("%d", &num);
 			if(num == 497)
 			    printf("\nNumero em octal: %d", h);
 			break;
-		}
+		default
+			if(opcao != 8)
+				printf("\nOpcao Invalida.");
 	}
 	return 0;
 }
