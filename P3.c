aa20a94743341716f5241df5b1f95035d2b84fc3
MateusSTL <42557847+MateusSTL@users.noreply.github.com>
2018-10-13 15:28:49 -0300

    Add files via upload

---
+++ P3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+	char numero[20];
+	int i;
+	fgets(numero, 20, stdin);
+	for(i = 0; numero[i] != '\0'; i++)
+	{
+		if(numero[i] <= ('9' - '0') && numero[i] >= ('0' - '0'))
+		    printf("%s", numero[i]);
+	}
+	return 0;
+}
