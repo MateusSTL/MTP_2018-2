92b12039edf57422fe97c3a523255118a812d797
MateusSTL <42557847+MateusSTL@users.noreply.github.com>
2018-11-11 18:13:57 -0200

    Add files via upload

---
+++ P6.c
@@ -0,0 +1,33 @@
+/* P6.c */
+/* Mateus Santos Tornilio Lemos */
+/* 11811EEL006 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define pi 3.14159265359
+#define r 1
+
+typedef struct 
+{
+  float x, y;
+}Ponto;
+
+int main()
+{
+   int N, i;
+   Ponto *xy;
+   scanf("%i", &N);
+   getchar();
+   xy= (Ponto *) malloc(N*sizeof(Ponto));
+   printf("%i retorna \" ",N);
+   for(i=0; i<N; i++)
+   {
+        xy->x= r*cos(i*2*pi/(N));
+        xy->y= r*sin(i*2*pi/N);
+        printf("(%.3f, %.3f) ", xy->x, xy->y);
+   }
+   printf("\" ");
+   free(xy);
+   return 0;
+}
